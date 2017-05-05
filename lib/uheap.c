bc9fe779012a48e9e644363c404d91132a8fe3a9
HassanAzzam <hassanazzam95@egmail.com>
2017-05-05 10:34:49 +0200

    Garrb kda

---
+++ lib/uheap.c
@@ -0,0 +1,198 @@
+
+#include <inc/lib.h>
+
+// malloc()
+//	This function use FIRST FIT strategy to allocate space in heap
+//  with the given size and return void pointer to the start of the allocated space
+
+//	To do this, we need to switch to the kernel, allocate the required space
+//	in Page File then switch back to the user again.
+//
+//	We can use sys_allocateMem(uint32 virtual_address, uint32 size); which
+//		switches to the kernel mode, calls allocateMem(struct Env* e, uint32 virtual_address, uint32 size) in
+//		"memory_manager.c", then switch back to the user mode here
+//	the allocateMem function is empty, make sure to implement it.
+
+
+//==================================================================================//
+//============================ REQUIRED FUNCTIONS ==================================//
+//==================================================================================//
+
+#define frame_no_start(x) (x-USER_HEAP_START)/PAGE_SIZE
+#define frame_no_end(x) (USER_HEAP_MAX-x)/PAGE_SIZE
+#define total_frames frame_no_end(USER_HEAP_START)
+#define va_frame(x) USER_HEAP_START + x*PAGE_SIZE
+int HEAP_INIT = 0;
+
+uint32 user_heap[total_frames];
+void Init_User_Heap(){
+    HEAP_INIT = 1;
+    for(int i = 0; i < total_frames; i++)
+    {
+        user_heap[i] = total_frames - i;
+    }
+}
+
+void* malloc(uint32 size)
+{
+    if(!HEAP_INIT)
+    {
+        Init_User_Heap();
+    }
+    
+    size /= PAGE_SIZE;
+    for(int i = 0; i < total_frames; i++)
+    {
+        if(user_heap[i] >= size)
+        {
+            sys_allocateMem(va_frame(i), size);
+            for(int j = i+size-1; j >= i; j--)
+            {
+                user_heap[j] = j - (i+size);
+            }
+            return (void *)va_frame(i);
+        }
+    }
+    return NULL;
+}
+
+void* smalloc(char *sharedVarName, uint32 size, uint8 isWritable)
+{
+	//TODO: [PROJECT 2017 - [6] Shared Variables: Creation] smalloc() [User Side]
+	// Write your code here, remove the panic and write your code
+	panic("smalloc() is not implemented yet...!!");
+
+	// Steps:
+	//	1) Implement FIRST FIT strategy to search the heap for suitable space
+	//		to the required allocation size (space should be on 4 KB BOUNDARY)
+	//	2) if no suitable space found, return NULL
+	//	 Else,
+	//	3) Call sys_createSharedObject(...) to invoke the Kernel for allocation of shared variable
+	//		sys_createSharedObject(): if succeed, it returns the ID of the created variable. Else, it returns -ve
+	//	4) If the Kernel successfully creates the shared variable, return its virtual address
+	//	   Else, return NULL
+
+	//This function should find the space of the required range
+	// ******** ON 4KB BOUNDARY ******************* //
+
+	//Use sys_isUHeapPlacementStrategyFIRSTFIT() to check the current strategy
+
+	//change this "return" according to your answer
+	return 0;
+}
+
+void* sget(int32 ownerEnvID, char *sharedVarName)
+{
+	//TODO: [PROJECT 2017 - [6] Shared Variables: Get] sget() [User Side]
+	// Write your code here, remove the panic and write your code
+	//panic("sget() is not implemented yet...!!");
+
+	// Steps:
+	//	1) Get the size of the shared variable (use sys_getSizeOfSharedObject())
+	//	2) If not exists, return NULL
+	//	3) Implement FIRST FIT strategy to search the heap for suitable space
+	//		to share the variable (should be on 4 KB BOUNDARY)
+	//	4) if no suitable space found, return NULL
+	//	 Else,
+	//	5) Call sys_getSharedObject(...) to invoke the Kernel for sharing this variable
+	//		sys_getSharedObject(): if succeed, it returns the ID of the shared variable. Else, it returns -ve
+	//	6) If the Kernel successfully share the variable, return its virtual address
+	//	   Else, return NULL
+	//
+
+	//This function should find the space for sharing the variable
+	// ******** ON 4KB BOUNDARY ******************* //
+
+	//Use sys_isUHeapPlacementStrategyFIRSTFIT() to check the current strategy
+
+	//change this "return" according to your answer
+	return 0;
+}
+
+// free():
+//	This function frees the allocation of the given virtual_address
+//	To do this, we need to switch to the kernel, free the pages AND "EMPTY" PAGE TABLES
+//	from page file and main memory then switch back to the user again.
+//
+//	We can use sys_freeMem(uint32 virtual_address, uint32 size); which
+//		switches to the kernel mode, calls freeMem(struct Env* e, uint32 virtual_address, uint32 size) in
+//		"memory_manager.c", then switch back to the user mode here
+//	the freeMem function is empty, make sure to implement it.
+
+void free(void* virtual_address)
+{
+    int start = frame_no_start((uint32)virtual_address);
+    int end = start-1 + user_heap[start]*-1;
+    
+    sys_freeMem((uint32)virtual_address, end - start + 1);
+    if(end == total_frames - 1 || user_heap[end+1] < 0 )
+    {
+        for(int i=end; i>=start; i--)
+            user_heap[i] *= -1;
+    }
+    else
+    {
+        for(int i=end; i>=start; i--)
+            user_heap[i] = user_heap[i+1] + 1;
+    }
+    
+    for(int i=start-1; i>=0 && user_heap[i] > 0; i--)
+        user_heap[i] = user_heap[i+1] + 1;
+    
+	//you should get the size of the given allocation using its address
+	//you need to call sys_freeMem()
+	//refer to the project presentation and documentation for details
+}
+
+
+//==================================================================================//
+//============================== BONUS FUNCTIONS ===================================//
+//==================================================================================//
+
+//=============
+// [1] sfree():
+//=============
+//	This function frees the shared variable at the given virtual_address
+//	To do this, we need to switch to the kernel, free the pages AND "EMPTY" PAGE TABLES
+//	from main memory then switch back to the user again.
+//
+//	use sys_freeSharedObject(...); which switches to the kernel mode,
+//	calls freeSharedObject(...) in "shared_memory_manager.c", then switch back to the user mode here
+//	the freeSharedObject() function is empty, make sure to implement it.
+
+void sfree(void* virtual_address)
+{
+	//TODO: [PROJECT 2017 - BONUS4] Free Shared Variable [User Side]
+	// Write your code here, remove the panic and write your code
+	panic("sfree() is not implemented yet...!!");
+
+	//	1) you should find the ID of the shared variable at the given address
+	//	2) you need to call sys_freeSharedObject()
+
+}
+
+
+//===============
+// [2] realloc():
+//===============
+
+//	Attempts to resize the allocated space at "virtual_address" to "new_size" bytes,
+//	possibly moving it in the heap.
+//	If successful, returns the new virtual_address, in which case the old virtual_address must no longer be accessed.
+//	On failure, returns a null pointer, and the old virtual_address remains valid.
+
+//	A call with virtual_address = null is equivalent to malloc().
+//	A call with new_size = zero is equivalent to free().
+
+//  Hint: you may need to use the sys_moveMem(uint32 src_virtual_address, uint32 dst_virtual_address, uint32 size)
+//		which switches to the kernel mode, calls moveMem(struct Env* e, uint32 src_virtual_address, uint32 dst_virtual_address, uint32 size)
+//		in "memory_manager.c", then switch back to the user mode here
+//	the moveMem function is empty, make sure to implement it.
+
+void *realloc(void *virtual_address, uint32 new_size)
+{
+	//TODO: [PROJECT 2017 - BONUS3] User Heap Realloc [User Side]
+	// Write your code here, remove the panic and write your code
+	panic("realloc() is not implemented yet...!!");
+
+}
